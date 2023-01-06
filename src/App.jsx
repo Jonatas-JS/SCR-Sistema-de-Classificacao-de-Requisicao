b590a0c07eb51f78725f294c563246ae0d0c802b
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-01-06 17:09:18 -0300

    login page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { LoginApp } from './components/LoginApp'
+
+function App() {
+
+  return (
+    <div className="App">
+      <LoginApp />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/LoginApp.jsx
@@ -0,0 +1,30 @@
+import styles from './LoginApp.module.css'
+import { Scroll, UserCircle } from 'phosphor-react'
+
+export function LoginApp() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoSpace}>
+        <h1>SCR</h1>
+        <div className={styles.iconScr}><Scroll size={78} />
+          <div className={styles.srcIcon}>Sistema de <br/> Classificação de<br/> Requisições</div>
+        </div>
+      </div>
+      <div className={styles.loginSpace}>
+        <div className={styles.titleLogin}>
+          <UserCircle size={30} weight="bold"/>
+          <h3>Login</h3>
+        </div>
+
+        <div className={styles.inputsLogin}>
+          <p>Usuário</p>
+          <input type="text" name="passwordLogin" id="userLogin"/>
+          <p>Senha</p>
+          <input type="password" name="passwordLogin" id="passwordLogin" />
+        </div>
+
+          <button type="submit">ENTRAR</button>
+      </div>
+    </div>
+  )
+}
