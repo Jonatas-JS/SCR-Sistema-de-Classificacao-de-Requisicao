51334520a8d3e025c7f9a592d4f0daa574627515
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-01-23 17:17:18 -0300

    Alinhamento HeaderLayer

--- src/pages/request/RequestPage.jsx
+++ src/pages/request/RequestPage.jsx
@@ -1,13 +1,18 @@
 import styles from './RequestPage.module.css'
 
+import { ListPlus } from 'phosphor-react'
 import { TitleLayer } from '../../components/TitleLayer';
+import { HeaderLayer } from '../../components/HeaderLayer';
 
 function RequestPage() {
   return (
     <div>
         <TitleLayer />
       <div className={styles.container}>
-        <h1>RequestPage</h1>
+        <HeaderLayer 
+        icon={<ListPlus size={36} weight='bold'/>}
+        name='Cadastrar Requisição'
+        />
       </div>
     </div>
   )
