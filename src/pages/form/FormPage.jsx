a91f1e7ca847c5a26402b8b37589f1a95ca82a47
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-01-27 17:03:15 -0300

    init layout form

--- src/pages/form/FormPage.jsx
+++ src/pages/form/FormPage.jsx
@@ -1,13 +1,54 @@
 import styles from './FormPage.module.css'
 
+import { CircleWavyCheck, Star } from 'phosphor-react'
 import { TitleLayer } from '../../components/TitleLayer';
+import { HeaderLayer } from '../../components/HeaderLayer'
 
 function FormPage() {
   return (
     <div>
         <TitleLayer />
       <div className={styles.container}>
-        <h1>FormPage</h1>
+        <HeaderLayer 
+        icon={<CircleWavyCheck size={36} weight='bold' />}
+        name='Formulário de Satisfação'
+        />
+
+        <div className={styles.formHeader}>
+          <p>*preencha o formulário de acordo com a sua experiência em relação aos serviços prestados pelo setor referente a essa requisição.</p>
+          <table>
+            <tr>
+              <th>Requisição: </th>
+              <td>5</td>
+            </tr>
+            <tr>
+              <th>Setor Avaliado: </th>
+              <td>TRANSPORTE</td>
+            </tr>
+          </table>
+
+        </div>
+
+        <div className={styles.formAvaliacao}>
+          <div className={styles.headerLayer}>
+            <HeaderLayer 
+            icon={<CircleWavyCheck size={28} />}
+            name='AVALIAÇÃO'
+          />
+          </div>
+
+          <div>
+            <p>De 0 (zero) a 5 (cinco), como foi sua experiência com os nossos serviços?</p>
+            <div className={styles.formAvaliations}>
+              <label>Atendimento: </label>
+              <Star size={22} />
+              <Star size={22} />
+              <Star size={22} />
+              <Star size={22} />
+              <Star size={22} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
