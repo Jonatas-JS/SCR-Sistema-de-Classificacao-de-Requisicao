3b45ed27b0c660573774f09cfe999601465f29a6
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-02-14 13:05:33 -0300

    configuration table

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,13 +1,61 @@
 import styles from './DashboardPage.module.css'
 
 import { TitleLayer } from '../../components/TitleLayer';
+import { HeaderLayer } from '../../components/HeaderLayer';
+import { Gauge, PencilSimpleLine, Trash } from 'phosphor-react'
 
 function DashboardPage() {
   return (
     <div>
         <TitleLayer />
       <div className={styles.container}>
-        <h1>DashBoard</h1>
+        <HeaderLayer 
+        icon={<Gauge size={36} weight='bold'/>}
+        name='Dashboard de Acompanhamento'
+        />
+
+        <p>Filtrar de <input type="date" name="" id="" /> até <input type="date" name="" id="" /></p>
+
+        <table className={styles.tableDashboard}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Status</th>
+                <th>Empresa</th>
+                <th>Cliente</th>
+                <th>Responsável</th>
+                <th>NF</th>
+                <th>Setor</th>
+                <th>Nota</th>
+                <th>Requisição</th>
+                <th>Conclusão</th>
+                <th className={styles.theadButons}>
+                <PencilSimpleLine
+                onClick={() => console.log('Editar')}
+                size={20} weight='bold' />
+                <Trash
+                onClick={() => console.log('Excluir')}
+                size={20} weight='bold' />
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {[...Array(6)].map((tr, i) => {
+              return (
+                <tr className={styles.trBody}>
+                  {[...Array(11)].map((tb, i) => {
+                    return (
+                      <td>{i}</td>
+                    )
+                  })}
+                </tr>
+              )
+              })}
+            </tbody>
+
+
+        </table>
       </div>
     </div>
   )
