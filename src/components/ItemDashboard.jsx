28ece5ef55a465634d860ad0abaf7f1ecdcffe23
Jonatas Fernandes da Silva <jonatas.tecnico19@gmail.com>
2023-03-02 17:16:30 -0300

    criate buttons for item

--- src/components/ItemDashboard.jsx
+++ src/components/ItemDashboard.jsx
@@ -1,13 +1,18 @@
 import styles from './ItemDashboard.module.css'
+import { PencilSimpleLine, Trash } from 'phosphor-react'
 
 export function ItemDashboard() {
   return (
       <tr className={styles.trBody}>
-        {[...Array(11)].map((tb, i) => {
+        {[...Array(10)].map((tb, i) => {
         return (
           <td>{i}</td>
         )
         })}
+        <td className={styles.buttonsAction}>
+          <PencilSimpleLine size={20} weight='bold' />
+          <Trash size={20} weight='bold' />
+        </td>
       </tr>
   )
 }
